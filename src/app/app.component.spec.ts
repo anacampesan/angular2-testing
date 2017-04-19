4fef717a9eeed49c458895fd57c136526e2af9fe
Ana Raquel Campesan <anazard@outlook.com>
2017-04-19 15:15:01 -0400

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
   it('should create the app', async(() => {
@@ -28,5 +34,36 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));
+
+  it('should render a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1')).toBeTruthy();
+  }));
+
+  it('should contain a p tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p')).toBeTruthy();
+    expect(compiled.querySelector('p').textContent).toContain('yo');
+  }));
+
+  it('should increase the value of the var if button is clicked', async(() => {
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const compiled = fixture.debugElement.nativeElement; // access html elements
+    // const app = fixture.componentInstance; // access properties and methods
+    component.increaseValue();
+    expect(component.value).toBe(1);
+  }));
+
+  it('should call the function uppon button click', async(() => {
+    spyOn(component, 'increaseValue');
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.increaseValue).toHaveBeenCalled();
+    })
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'app works!';
+  value: number = 0;
+
+  increaseValue() {
+    this.value++;
+  }
 }
